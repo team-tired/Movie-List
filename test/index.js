236c502b219100200560477f810d8e3eb3dfdf63
DannySuarez <suarezd10@gmail.com>
2019-06-03 14:47:46 -0700

    added lines for travis to be happy

--- test/index.js
+++ test/index.js
@@ -10,6 +10,8 @@ global.window = window;
 global.document = window.document;
 global.FormData = window.FormData;
 global.localStorage = window.localStorage;
+global.sessionStorage = window.sessionStorage;
+global.URLSearchParams = window.URLSearchParams;
 
 require = require('esm')(module);
 module.exports = require('./tests.js');
