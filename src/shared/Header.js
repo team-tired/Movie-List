c88dbe9d67beb75dc81a3cd995560817684c60e9
DannySuarez <suarezd10@gmail.com>
2019-06-03 14:29:36 -0700

    rendered profile component into header component

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,12 +1,24 @@
 import Component from '../Component.js';
+import Profile from './Profile.js';
+import { auth } from '../services/firebase.js';
 
 class Header extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+        const profile = new Profile();
+        dom.appendChild(profile.render());
+
+        auth.onAuthStateChanged(user => {
+            profile.update({ user });
+        });
+    }
     renderTemplate() {
-        // const title = this.props.title || document.title;
+        const title = this.props.title || document.title;
 
         return /*html*/`
             <header>
-                <h1>Hello World</h1>
+                <h1>${title}</h1>
             </header>
         `;
     }
