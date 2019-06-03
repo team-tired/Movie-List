7404d11e2cba9d6571aea8a57cf49a3b69d03eb2
DannySuarez <suarezd10@gmail.com>
2019-06-03 14:28:17 -0700

    created html-equal.js  file

--- test/html-equal.js
+++ test/html-equal.js
@@ -2,11 +2,11 @@ const pattern = /[\f\n\r\t\v ]{2,}/g;
 const replacement = ' ';
 
 function normalize(html) {
-    // normalize whitespace, everything becomes one space
+   // normalize whitespace, everything becomes one space
     const replaced = html.replace(pattern, replacement);
-    // trim leading and trailing whitespace
+   // trim leading and trailing whitespace
     const trimmed = replaced.trim();
-    // return the result
+   // return the result
     return trimmed;
 }
 
