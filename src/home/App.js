bfef57d78cc47571e25864cd2829c97b5153074f
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-04 14:52:37 -0700

    rendered search component in App and put event listener and render section into search component

--- src/home/App.js
+++ src/home/App.js
@@ -2,16 +2,20 @@ import Component from '../Component.js';
 import Header from '../shared/Header.js';
 import MovieList from '../shared/movieList.js';
 import movieApi from '../services/movie-api.js';
+import Search from './Search.js';
 
 class App extends Component {
 
     render() {
         const dom = this.renderDOM();
+        const main = dom.querySelector('main');
 
         const header = new Header({ title: 'Welcome to Watchy Things' });
         const headerDOM = header.render();
-        const main = dom.querySelector('main');
         dom.insertBefore(headerDOM, main);
+        
+        const search = new Search();
+        main.appendChild(search.render());
 
         const movieList = new MovieList({ movies: [] });
         main.appendChild(movieList.render());
