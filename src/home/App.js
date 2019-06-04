6cc4f300433ac85fdba179f10b4265d4341d92d7
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-04 13:47:39 -0700

    rendered in app

--- src/home/App.js
+++ src/home/App.js
@@ -1,5 +1,6 @@
 import Component from '../Component.js';
 import Header from '../shared/Header.js';
+import MovieList from '../shared/movieList.js';
 
 class App extends Component {
 
@@ -10,6 +11,9 @@ class App extends Component {
         const headerDOM = header.render();
         const main = dom.querySelector('main');
         dom.insertBefore(headerDOM, main);
+
+        const movieList = new MovieList({ movies: [] });
+        main.appendChild(movieList.render());
 
         return dom;
     }
