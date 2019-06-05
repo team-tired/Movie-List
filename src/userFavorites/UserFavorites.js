599bfff94fe19878776c4077a0655eb619b6268b
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-05 09:39:10 -0700

    rendered our header, movielist, and user favorites

--- src/userFavorites/UserFavorites.js
+++ src/userFavorites/UserFavorites.js
@@ -1,10 +1,34 @@
 import Component from '../Component.js';
+import Header from '../shared/Header.js';
+import MovieList from '../shared/MovieList.js';
+import { auth, userFavoritesRef } from '../services/firebase.js';
 
 class UserFavorites extends Component {
+    render() {
+        const dom = this.renderDOM();
+        const main = dom.querySelector('main');
+
+        const header = new Header({ title: 'Favorite Movies' });
+        dom.insertBefore(header.render(), main);
+
+        const movieList = new MovieList({ movies: [] });
+        main.appendChild(movieList.render());
+
+        userFavoritesRef
+            .child(auth.currentUser.uid)
+            .on('value', snapshot => {
+                const value = snapshot.val();
+                const movies = value ? Object.values(value) : [];
+                movieList.update({ movies });
+            });
+
+
+        return dom;
+    }
     renderTemplate() {
         return /*html*/`
         <div>
-            <main>TTTTTEEEESSSSTTT
+            <main>
             </main>
         </div>
         `;
