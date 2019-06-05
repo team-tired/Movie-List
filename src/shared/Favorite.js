329b65a0e4c833df0ccd666f08679d6a20368441
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-04 17:19:46 -0700

    added event listener for favorite button

--- src/shared/Favorite.js
+++ src/shared/Favorite.js
@@ -1,7 +1,13 @@
 import Component from '../Component.js';
 
 class Favorite extends Component {
-    
+    render() {
+        const button = this.renderDOM();
+        button.addEventListener('click', () => {
+            this.props.onFavorite(!this.props.isFavorite);
+        });
+        return button;
+    }
     
     renderTemplate() {
         const isFavorite = this.props.isFavorite;
