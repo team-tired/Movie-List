4e5e8926c5aad798f92c7e54835f8319294fcfc3
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-04 17:20:14 -0700

    began wirting data to firebase

--- src/shared/MovieItem.js
+++ src/shared/MovieItem.js
@@ -1,12 +1,29 @@
 import Component from '../Component.js';
 import Favorite from './Favorite.js';
+import { auth, userFavoritesRef } from '../services/firebase.js';
 
 class MovieItem extends Component {
     render() {
         const dom = this.renderDOM();
         const container = dom.querySelector('.favorite-container');
+        const movies = this.props.movies;
+    
+        const userMovieRef = userFavoritesRef
+            .child(auth.currentUser.uid)
+            .child(movies.id);
 
-        const favorite = new Favorite();
+        const favorite = new Favorite({
+            isFavorite: false,
+            onFavorite: (makeFavorite) => {
+                if(makeFavorite) {
+                    userMovieRef.set({
+                        id: movies.id,
+                        title: movies.title,
+                        photo: movies.poster_path
+                    });
+                }
+            }
+        });
         container.appendChild(favorite.render());
 
         return dom;
