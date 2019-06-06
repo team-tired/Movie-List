93ff577ca23fc802f4e55e6879541a15cfa1002d
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-06 11:37:09 -0700

    added render section

--- src/movie/MovieDetail.js
+++ src/movie/MovieDetail.js
@@ -1,6 +1,32 @@
 import Component from '../Component.js';
+import { updateFavorites, getUserFavoritesRef } from '../services/actions.js';
+import Favorite from '../shared/Favorite.js';
 
 class MovieDetail extends Component {
+    render() {
+        const dom = this.renderDOM();
+        const container = dom.querySelector('.favorite-container');
+        const movies = this.props.movies;
+    
+        const id = movies.id;
+        const userMovieRef = getUserFavoritesRef(id);
+
+        const favorite = new Favorite({
+            isFavorite: false,
+            onFavorite: (makeFavorite) => {
+                updateFavorites(movies, makeFavorite, userMovieRef);
+            }
+        });
+        container.appendChild(favorite.render());
+
+        userMovieRef.on('value', snapshot => {
+            const isFavorite = Boolean(snapshot.val());
+            favorite.update({ isFavorite });
+        });
+
+        return dom;
+    }
+
     renderTemplate() {
         const movie = this.props.movie;
         if(!movie) {
