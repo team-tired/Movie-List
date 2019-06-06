567ce516f1f42b55e42951a6bc6ae17c2616227e
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-05 17:14:30 -0700

    added the header to movieapp

--- src/movie/MovieApp.js
+++ src/movie/MovieApp.js
@@ -1,11 +1,20 @@
 import Component from '../Component.js';
+import Header from '../shared/Header.js';
 
 class MovieApp extends Component {
+    render() {
+        const dom = this.renderDOM();
+
+        const header = new Header();
+        const main = dom.querySelector('main');
+        dom.insertBefore(header.render(), main);
+
+        return dom;
+    }
     renderTemplate() {
         return /*html*/`
             <div>
                 <main>
-                Test for funsies
                 <main>
             </div>
         `;
