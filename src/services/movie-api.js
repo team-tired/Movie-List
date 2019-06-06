67a48cf860d3d4201e8625703869735773654de6
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-06 10:26:57 -0700

    changed url variable names

--- src/services/movie-api.js
+++ src/services/movie-api.js
@@ -2,21 +2,21 @@ import key from '../../configs.js';
 
 //https://api.themoviedb.org/3/movie/420817?api_key=7b3c36453f97c3077a7a10fd62585119&language=en-US
 
-const DETAIL_URL = `https://api.themoviedb.org/3`;
+const BASE_URL = `https://api.themoviedb.org/3`;
 
 const preKey = `?api_key=`;
 
-const URL = `${DETAIL_URL}/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+const DISCOVER_URL = `${BASE_URL}/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
 
 const movieApi = {
     getMovies(query) {
         if(query) {
-            return fetch(`${DETAIL_URL}/search/movie?api_key=${key}&language=en-US&query=${query}&page=1&include_adult=false`).then(response => response.json());
+            return fetch(`${BASE_URL}/search/movie?api_key=${key}&language=en-US&query=${query}&page=1&include_adult=false`).then(response => response.json());
         }
-        return fetch(URL).then(response => response.json());
+        return fetch(DISCOVER_URL).then(response => response.json());
     },
     getMovie(id) {
-        const url = `${DETAIL_URL}/movie/${id}${preKey}${key}`;
+        const url = `${BASE_URL}/movie/${id}${preKey}${key}`;
 
         return fetch(url).then(response => response.json());
     }
