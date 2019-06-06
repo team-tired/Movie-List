a7b40dbba7c43701ae13c58baee7c8d1ff3313e4
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-06 11:18:36 -0700

    made another function our of userfavoritesreference

--- src/services/actions.js
+++ src/services/actions.js
@@ -1,5 +1,16 @@
+import { auth, userFavoritesRef } from './firebase.js';
 
-export function updateFavorites(movies, makeFavorite, userMovieRef) {
+function getUserFavoritesRef(id) {
+    const userMovieRef = userFavoritesRef
+        .child(auth.currentUser.uid)
+        .child(id);
+    return userMovieRef;
+}
+
+export function updateFavorites(movies, makeFavorite) {
+    const id = movies.id;
+    const userMovieRef = getUserFavoritesRef(id);
+            
     if(makeFavorite) {
         userMovieRef.set({
             id: movies.id,
